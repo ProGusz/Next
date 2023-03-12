a7c98c86b4f94c084a4873548efccd6d13015ab4
progusz <kawinpongpluem@gmail.com>
2023-03-12 22:37:23 +0700

    first commit

---
+++ pages/about/aaa.js
@@ -0,0 +1,12 @@
+export default function about() {
+    return (
+        
+      <div>
+        <h3>this is about page</h3> 
+        <a href="/port">port</a>
+  
+  
+      </div>
+  
+    )
+    }
---
+++ pages/port/index.js
@@ -0,0 +1,10 @@
+export default function port() {
+    return (
+      <div>
+        <h3>this is port page</h3> 
+  
+  
+      </div>
+  
+    )
+    }
